56b82a5e4812203175ae113d63e123832f798efb
AlexVYard <alexvegayard@gmail.com>
2023-08-02 16:56:16 -0500

    eslint rules updated

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const { MongoClient } = require("mongodb");
 // Replace the uri string with your connection string.
-const config = require("./config");
+const config = require("./config")
 
-const uri = config.dbUrl;
-const client = new MongoClient(uri);
+const uri = config.dbUrl
+const client = new MongoClient(uri)
+
 async function run() {
   try {
-    const database = client.db();
+    const database = client.db()
     /* const movies = database.collection('movies');
     // Query for a movie that has the title 'Back to the Future'
     const query = { title: 'Back to the Future' };
     const movie = await movies.findOne(query); */
-    console.log(database);
+    console.log(database)
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    await client.close()
   }
 }
-run().catch(console.dir);
+
+run().catch(console.dir)
