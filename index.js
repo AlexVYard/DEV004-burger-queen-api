6057a33ac1542019e828dc377f7e9844a9ca615e
AlexVYard <alexvegayard@gmail.com>
2023-08-02 17:34:08 -0500

    connect.js ready

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { MongoClient } = require("mongodb");
-// Replace the uri string with your connection string.
+const { MongoClient } = require("mongodb")
+
 const config = require("./config")
 
 const uri = config.dbUrl
@@ -7,12 +7,13 @@ const client = new MongoClient(uri)
 
 async function run() {
   try {
-    const database = client.db()
+    const database = await client.db("sample_airbnb")
+    const collection = await database.collection("listingsAndReviews")
     /* const movies = database.collection('movies');
     // Query for a movie that has the title 'Back to the Future'
     const query = { title: 'Back to the Future' };
     const movie = await movies.findOne(query); */
-    console.log(database)
+    console.log(collection)
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close()
