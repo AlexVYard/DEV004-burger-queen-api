82c73bebf283467f8b972fa00cff948a4782db2a
AlexVYard <alexvegayard@gmail.com>
2023-08-07 17:14:13 -0500

    displays name and version when accesing main route

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const config = require('./config');
 const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
 const routes = require('./routes');
-// const pkg = require('./package.json');
+const pkg = require('./package.json');
 
 const { port, secret } = config;
 const app = express();
 
 app.set('config', config);
-// app.set('pkg', pkg);
+app.set('pkg', pkg);
 
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
@@ -28,8 +28,3 @@ routes(app, (err) => {
     console.info(`App listening on port ${port}`);
   });
 });
-
-// Agregado para probar index.js
-/* app.listen(port, () => {
-  console.info(`App listening on port ${port}`);
-}); */
