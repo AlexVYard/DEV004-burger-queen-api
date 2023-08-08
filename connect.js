588a9093bdfcbf179cd830a8b30ba9dc7e268a13
AlexVYard <alexvegayard@gmail.com>
2023-08-08 16:03:40 -0500

    added error to connect
    connecting from index.js

--- connect.js
+++ connect.js
@@ -9,13 +9,15 @@ const client = new MongoClient(uri)
 async function connect() {
   // TODO: Conexión a la Base de Datos
   try {
-    const database = await client.db("sample_airbnb")
-    const collection = await database.collection("listingsAndReviews")
+    await client.db()
+    // const collection = await database.collection("listingsAndReviews")
     /* const movies = database.collection('movies');
     // Query for a movie that has the title 'Back to the Future'
     const query = { title: 'Back to the Future' };
     const movie = await movies.findOne(query); */
-    console.log(collection)
+    console.log("Connected to database")
+  } catch (error) {
+    console.error(error)
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close()
