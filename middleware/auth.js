85e72d68ea6d6cfc8f66554b2e0118255417c003
AlexVYard <alexvegayard@gmail.com>
2023-08-20 13:59:56 -0500

    decoding "token" using "secret"
    // requests.http: authorization modified
    // check if there is "authorization"
    // separate "authorization" into "type" and "token"
    // check if type is "bearer"

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,25 +3,42 @@ const jwt = require('jsonwebtoken');
 module.exports = (secret) => (req, resp, next) => {
   const { authorization } = req.headers;
 
-  // console.log("req.body", req.body)
+  // console.log("secret", secret)
   // console.log("resp", resp)
-  console.log("authorization:", authorization)
+  // console.log("authorization:", authorization)
 
-  // si no hay autorizacion
+  // check if there is "authorization"
   if (!authorization) {
     return next();
   }
 
+  // separate "authorization" into "type" and "token"
   const [type, token] = authorization.split(' ');
 
+  // console.log("type:", type)
+  // console.log("token:", token)
+ 
+  // check if type is "bearer"
   if (type.toLowerCase() !== 'bearer') {
     return next();
   }
 
+  // console.log("jwt:", jwt.verify())
+
+  // var parts = token.split('.')
+  // console.log("parts:", parts)
+
+  // decoding "token" using "secret"
   jwt.verify(token, secret, (err, decodedToken) => {
+    console.log("token:", token)
+    console.log("secret:", secret)
+    console.log("decodedToken:", decodedToken)
     if (err) {
+      console.log("err:", err)
       return next(403);
     }
+
+    console.log("decodedToken.uid:", decodedToken.uid)
 
     // TODO: Verificar identidad del usuario usando `decodeToken.uid`
   });
